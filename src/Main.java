68f997a7137ef3c6d47dad485d8f17b18472078d
Jakub Chochołowicz <j.chocholowicz@gmail.com>
2023-03-16 21:51:53 +0100

    add feature-e

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 10; // przykładowa wartość n
+        int sum = sumOfFirstN(n);
+
+        System.out.println("Suma " + n + " kolejnych liczb naturalnych wynosi " + sum);
+    }
+
+    private static int sumOfFirstN(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        return sum;
     }
 }
